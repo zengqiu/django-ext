4625aa027ed2b9213857a7bac12e64b9dc81b220
zengqiu <zengqiu@qq.com>
2023-04-13 16:41:54 +0800

    Update locale

--- django_ext/models.py
+++ django_ext/models.py
@@ -4,24 +4,24 @@ from django_currentuser.db.models import CurrentUserField
 
 
 class TimestampModel(models.Model):
-    created_at = models.DateTimeField(_('Created at'), auto_now_add=True)
-    updated_at = models.DateTimeField(_('Updated at'), auto_now=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
     class Meta:
         abstract = True
 
 
 class TimeRangeModel(models.Model):
-    started_at = models.DateTimeField(_('Started at'), null=True, blank=True)
-    ended_at = models.DateTimeField(_('Ended at'), null=True, blank=True)
+    started_at = models.DateTimeField(_('started at'), null=True, blank=True)
+    ended_at = models.DateTimeField(_('ended at'), null=True, blank=True)
 
     class Meta:
         abstract = True
 
 
 class UserTrackModel(models.Model):
-    created_by = CurrentUserField(on_delete=models.PROTECT, related_name='+', verbose_name=_('Created by'))
-    updated_by = CurrentUserField(on_delete=models.PROTECT, on_update=True, related_name='+', verbose_name=_('Updated by'))
+    created_by = CurrentUserField(on_delete=models.PROTECT, related_name='+', verbose_name=_('created by'))
+    updated_by = CurrentUserField(on_delete=models.PROTECT, on_update=True, related_name='+', verbose_name=_('updated by'))
 
     class Meta:
         abstract = True
